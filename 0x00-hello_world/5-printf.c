e6695dbcb2e36b09c0cb434698271d34dbc86bf7
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-15 06:10:26 -0700

    printf coommit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry of every program
  *
- * Return: Always in 0
+ * Return: Always written in 0
  */
 
 int main(void)
 {
 
-	Printf("with proper grammar, but the outcome is a piece of art,\n");
-
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
 }
