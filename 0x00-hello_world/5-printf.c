35bade32bf7af1d3aa2958fada5766644da47484
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-15 06:20:44 -0700

    printed commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry of every program
+ * main - Entry point of every program
  *
  * Return: Always written in 0
  */
-
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
+
 	return (0);
 }
