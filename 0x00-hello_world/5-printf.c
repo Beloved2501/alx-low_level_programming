b86a2b622042df4eb1acf2cc7c20b84f81a9ae0c
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-15 06:02:05 -0700

    print command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,11 @@
  *
  * Return: Always in 0
  */
+
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
+}
