b1458754984cef4bb4715be89649564ee67c88bb
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-16 09:19:48 -0700

    third alphabet commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for all program
+ *
+ * Return: Always in 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
