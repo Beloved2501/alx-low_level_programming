af6097751907abd2c807d75af28d71ad315bc1fa
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-20 13:00:49 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,19 +18,27 @@ void print_to_98(int n)
 		for (a = n; a <= 98; a++)
 		{
 			if (a != 98)
+			{
 				printf("%d, ", a);
+			}
 			else if (a == 98)
+			{
 				printf("%d\n", a);
+			}
 		}
 	}
 	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
 		{
-			for (b = n; b >= 98; b--)
+			if (b != 98)
 			{
-				if (b != 98)
-					printf("%d, ", b);
-				else if (b == 98)
-					printf("%d\n", b);
+				printf("%d, ", b);
 			}
-		}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+			}
+			}
+	}
 }
