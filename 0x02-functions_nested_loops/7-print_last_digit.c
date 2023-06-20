128e68108bab171dbeaf39806091b7e2eac5f93a
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-20 08:25:37 -0700

    last digit printed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,11 @@ int print_last_digit(int n)
 	int j;
 
 	j = n % 10;
+
 	if (j < 0)
 	{
-		_putchar(-j + 48);
-		return (-j);
-	}
-	else
-	{
-		_putchar(j - 48);
+		_putchar(j * = -1);
+
 		return (j);
 	}
 }
