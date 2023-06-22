10f26ca50834e5433759ac491175056de9f54d70
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-21 20:30:19 -0700

    Fibonacci message

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Displays first 50 fibonacci numbers,
+ * starting with 1 and 2,
+ * separated by a comma followed by a space
+ *
+ * Return: Always in 0
+ */
+int main(void)
+{
+	int j;
+	unsigned long a = 0, b = 1, sum;
+
+	for (j = 0; j < 50; j++)
+	{
+		sum = a + b;
+		printf("lu", sum);
+
+		a = b;
+		b = sum;
+
+		if (j == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
