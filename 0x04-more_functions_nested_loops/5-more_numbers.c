d5cfb42dbe5e32bd77bf988ca6bf5824fdc7d382
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-22 08:51:25 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * more_numbers - displays numbers in 10 times
+ *
+ * Return: Return void
+ */
+void more_numbers(void)
+{
+	int j;
+	int k;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+				_putchar(k % 10 + '0');
+		}
+			_putchar('\n');
+	}
+}
