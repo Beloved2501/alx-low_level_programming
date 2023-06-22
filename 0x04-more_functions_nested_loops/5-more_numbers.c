f207fd64d5b13e5a3c2f73b50e4ea769f4b392cd
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-22 09:31:57 -0700

    more new numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
 {
 	int j;
 	char k;
-
+	
 	for (j = 0; j < 10; j++)
 	{
 		for (k = 0; k <= 14; k++)
 		{
 			if (k > 9)
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
