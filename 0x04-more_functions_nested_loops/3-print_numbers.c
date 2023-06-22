0c1d9cfa376e8b6722671f7091a45713d5402aea
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-22 07:51:38 -0700

    printed numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - displays numbers from 0 to 9
+ *
+ * Return: Return void
+ */
+void print_numbers(void)
+{
+	char j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		_putchar(j);
+	}
+	{
+		_putchar('\n');
+	}
+}
