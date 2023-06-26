67081bce93621b4038671d7841270b55d6e8668b
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-26 12:49:06 -0700

    puts commits

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - display other character of string,
+ * followed by new line
+ * @str: pointer variable
+ * Return: Return void
+ */
+void puts2(char *str);
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
