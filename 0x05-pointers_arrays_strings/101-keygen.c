f2cde55c8ed6a964202fe650dc298a64f91b6942
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-27 14:39:06 -0700

    password generated

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - functiono generate random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always in 0
+ */
+int main(void)
+{
+	int code[100];
+	int a, addition, b;
+
+	addition = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+	{
+		code[a] = rand() % 78;
+		addition += (code[a] + '0');
+		_putchar(code[a] + '0');
+		if ((2772 - addition) - '0' < 78)
+		{
+			b = 2772 - addition - '0';
+			addition += b;
+			_putchar(b + '0');
+			break;
+		}
+	}
+	return (0);
+}
