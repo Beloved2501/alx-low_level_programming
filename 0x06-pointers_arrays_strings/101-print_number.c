537651191176fd87c69cd998ee00bb910d310635
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-29 13:01:21 -0700

    print number commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - functions that print integer
+ * @n: integer value
+ * Return: Always in 0
+ */
+void print_number(int n)
+{
+	unsigned int a1;
+
+	a1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		a1 = -n;
+	}
+	if (a1 / 10 != 0)
+	{
+		print_number(a1 / 10);
+	}
+	_putchar((a1 % 10) + '0');
+}
