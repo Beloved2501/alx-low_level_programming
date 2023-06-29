c75e6b2624bbe207149b2e740e9fcb0d618dff07
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-29 14:37:44 -0700

    magic file created

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/* main - Entry point of all program */
+
+/**
+ * main - print the magic numbers
+ *
+ * Return: Return 0
+ */
+
 int main(void)
 {
 	int j;
@@ -8,15 +14,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &j;
+
 	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only use one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-		*(p + 5) = 98;
+	* write your line of code here...
+	* Remember
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
+	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
