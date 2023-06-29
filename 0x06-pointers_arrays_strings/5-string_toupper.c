d08fa2cf00ef8259df6ef01ae941f7bbf361ef21
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-06-29 05:16:48 -0700

    upper to lower cases

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,19 +2,19 @@
 
 /**
  * string_toupper - converts all lowercase to uppercase
- * @p: variable pointer
- * Return: Return p
+ * @n: variable pointer
+ * Return: Return n
  */
-char *string_toupper(char *p)
+char *string_toupper(char *n)
 {
 	int a;
 
 	a = 0;
-	while (p[a] != '\0')
+	while (n[a] != '\0')
 	{
-		if (p[a] >= 'a' && p[a] >= 'z')
-			p[a] = p[a] - 32;
+		if (n[a] >= 'a' && n[a] >= 'z')
+			n[a] = n[a] - ' ';
 		a++;
 	}
-	return (p);
+	return (n);
 }
