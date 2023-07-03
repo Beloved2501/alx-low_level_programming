89b6eddaa8f9834c019e90fc8964c5a916991de3
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-03 06:53:12 -0700

    new chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,14 +8,13 @@ include "main.h"
  */
 void print_chessboard(char (*a)[8])
 {
-	int b;
-	int c;
+	int b, c;
 
 	for (b = 0; b < 8; b++)
 	{
 		for (c = 0; c < 8; c++)
 		{
-			_putchar(a[b][c]);
+		_putchar(a[b][c]);
 		}
 
 		_putchar('\n');
