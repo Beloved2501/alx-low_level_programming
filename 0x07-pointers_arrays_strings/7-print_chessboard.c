657d0b9cbaa187b106dd51e228a2782432ca229e
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-03 06:20:44 -0700

    chessboard commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard
+ * @a: 2d array of characters
+ *
+ * Return: Return void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+
+		_putchar('\n');
+	}
+}
