066d2c7d68b167311fe4bbf3e5606cba88310cd6
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-07 08:24:02 -0700

    multiplication of numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that multiplies two numbers
+ * @argc: argument counts
+ * @argv: list of array pointing to the string
+ * Return: Return product or 1
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, product;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
+	printf("%d\n", product);
+	return (0);
+}
