fd6b3351a77c4cb178bf5a3a95457513eec13e98
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-07 07:33:04 -0700

    first argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed into the string
+ * @argc: arguments counts
+ * @argv: list of array pointing to the string
+ * Return: Return void
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
