32c0b7d12a0f84972a47f11d689d9c62d23f7e83
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-07 08:41:01 -0700

    arguments multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,8 +5,7 @@
  * main - function that multiplies two numbers
  * @argc: argument counts
  * @argv: list of array pointing to the string
- * @atoi: convert strings to integer
- * Return: Return product or 1
+ * Return: Return product of multiplication or 1
  */
 int main(int argc, char *argv[])
 {
@@ -17,8 +16,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	/* atoi: converts strings to integer */
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
+
 	product = a * b;
 	printf("%d\n", product);
 	return (0);
