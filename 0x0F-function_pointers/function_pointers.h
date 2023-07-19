9d4990b4b3dcb7e0438c32fc748ddf8c01fb0766
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-19 04:11:56 -0700

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTIONPOINTER_H
+#define FUNCTIONPOINTER_H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
