ef9a06ae14470362c6e5a3314e3789784e11f791
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-27 06:18:01 -0700

    add nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,10 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
  * _strlen - length of the string
  * @s: string variable
- * Return: Return the length of string
- **/
+ * Return: length of the string
+ */
 int _strlen(const char *s)
 {
 	int j;
@@ -17,33 +14,33 @@ int _strlen(const char *s)
 	return (j);
 }
 /**
- * add_node - add new nodes at the beginning of a list
- * @head: pointer to the pointer to the head of list
- * @str: string to be duplicated and add to node
- * Return: address of new element or NULL if failed.
- **/
+ * add_node - add new nodes to the list
+ * @head: current place in the list
+ * @str: string to add to the head
+ * Return: number of element in list
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	int j, length;
-	char *ptr;
+	char *inside;
 	list_t *new_node;
 
 	if (str == NULL || head == NULL)
 		return (NULL);
 	length = _strlen(str);
 	new_node = *head;
-	ptr = malloc((length + 1) * sizeof(char));
-	if (ptr == NULL)
+	inside = malloc((length + 1) * sizeof(char));
+	if (inside == NULL)
 		return (NULL);
 	for (j = 0; str[j]; j++)
-		ptr[j] = str[j];
+		inside[j] = str[j];
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		free(ptr);
+		free(new_node);
 		return (NULL);
 	}
-	new_node->str = ptr;
+	new_node->str = inside;
 	new_node->len = length;
 	new_node->next = *head;
 	*head = new_node;
