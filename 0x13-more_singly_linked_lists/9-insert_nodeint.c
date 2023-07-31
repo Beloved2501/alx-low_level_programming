ec000a50a058896e712160c24f48cd2da1c05759
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-07-31 11:12:47 -0700

    insert list of nodes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *curr;
 	unsigned int j;
+
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
