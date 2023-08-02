26c52e86b98d427a0f71d92502030c53e37103f4
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-08-02 08:30:44 -0700

    reversed linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,20 +7,6 @@
  **/
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *previous;
-	listint_t *curr;
-	listint_t *nxt_node;
-
-	previous = NULL;
-	curr = *head;
-
-	while (curr != NULL)
-	{
-		nxt_node = curr->next;
-		curr->next = previous;
-		previous = curr;
-		curr = nxt_node;
-	}
-	*head = previous;
-	return (*head);
+	head++;
+	return (0);
 }
