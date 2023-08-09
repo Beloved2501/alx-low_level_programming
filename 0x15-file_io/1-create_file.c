3469e2abb54efdbfc713e84cf8fab729816354c8
Beloved2501 <oluwatosinyusuf92@gmail.com>
2023-08-08 23:55:14 -0700

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	file = open(filename, O_CTEAT | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	byte_write = write(file, text_content, len);
 
 	if (file == -1 || byte_write == -1)
